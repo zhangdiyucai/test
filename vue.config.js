580cdfbbc486eb384685336ee1c09bdff95dc481
zhangdiyucai <1619094517@qq.com>
2020-06-12 18:27:39 +0800

    dd

--- vue.config.js
+++ vue.config.js
@@ -92,23 +92,23 @@ module.exports = {
     }
   },
   // 所有 webpack-dev-server 的选项都支持
-  devServer: {
-    open: true,
-    host: 'localhost',
-    port: 3000,
-    https: false,
-    hotOnly: true,
-    // 配置代理服务器
-    proxy: {
-      // 凡是请求以api开头的都会使用下面的代理服务器
-      '/api/*': {
-        target: 'http://192.168.32.2:8899/', // 目标服务器地址
-        secure: false, // 目标服务器地址是否是安全协议
-        changeOrigin: true // 是否修改来源, 为true时会让目标服务器以为是webpack-dev-server发出的请求!服务端和服务端的请求是没有跨域的
-        // pathRewrite: {'^/api': '/'}     // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
-      }
-    }
-  },
+  // devServer: {
+  //   open: true,
+  //   host: 'localhost',
+  //   port: 3000,
+  //   https: false,
+  //   hotOnly: true,
+  //   // 配置代理服务器
+  //   proxy: {
+  //     // 凡是请求以api开头的都会使用下面的代理服务器
+  //     '/api/*': {
+  //       target: 'http://192.168.32.2:8899/', // 目标服务器地址
+  //       secure: false, // 目标服务器地址是否是安全协议
+  //       changeOrigin: true // 是否修改来源, 为true时会让目标服务器以为是webpack-dev-server发出的请求!服务端和服务端的请求是没有跨域的
+  //       // pathRewrite: {'^/api': '/'}     // 将/api开头的请求地址, /api 改为 /, 即 /api/xx 改为 /xx
+  //     }
+  //   }
+  // },
   // 第三方插件配置
   pluginOptions: {}
 }
